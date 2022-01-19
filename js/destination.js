573632efbaf5f7ec28713850065e78127d7ef159
achraf eddiba <69601953+achrafedd@users.noreply.github.com>
2022-01-19 18:20:27 +0100

    fix json path

--- js/destination.js
+++ js/destination.js
@@ -6,7 +6,7 @@ let distance = document.querySelector(".distance");
 let travel = document.querySelector(".travel");
 let elements = [];
 
-fetch("../data.json")
+fetch("https://achrafedd.github.io/Space-Tourism/data.json")
   .then((res) => res.json())
   .then((data) => {
     for (let i = 0; i < data.destinations.length; i++) {
