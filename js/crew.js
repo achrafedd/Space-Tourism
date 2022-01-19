d274e1900abc1f5f629e865cfadf5613608a9ffc
achraf eddiba <69601953+achrafedd@users.noreply.github.com>
2022-01-19 18:17:27 +0100

    fix json path

--- js/crew.js
+++ js/crew.js
@@ -5,7 +5,7 @@ let bio = document.querySelector(".bio");
 let image = document.querySelector(".image");
 let elements = [];
 
-fetch("../data.json")
+fetch("https://achrafedd.github.io/Space-Tourism/data.json")
   .then((res) => res.json())
   .then((data) => {
     for (let i = 0; i < data.crew.length; i++) {
